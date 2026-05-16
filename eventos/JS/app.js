b6a464da64a79bb3e75de5fb166a3964c14b6008
Alex <alexmonro5863@outlook.es>
2026-05-16 08:04:39 -0500

    eventos

--- eventos/JS/app.js
+++ eventos/JS/app.js
@@ -1,26 +1,36 @@
-
-const boton=document.querySelector('button');
+const boton=document.querySelector("button");
 
 /*boton.addEventListener('click', saludar);
 
 function saludar() {
-    console.log('Hola mundo');
+    console.log("Hola mundo");
 }
 
-/*boton.onclick = function saludar() {
-    console.log('Hola mundo');
-}*/
+boton.onclick = function saludar() {
+    console.log("Hola mundo");
+}
 
-/*boton.addEventListener('mouseover', function() {
+boton.addEventListener('mouseover', function() {
     boton.style.backgroundColor = 'red';
 });
 
 boton.removeEventListener('mouseover', function() {
     boton.style.backgroundColor = 'red';
-});*/
+});
+boton.addEventListener('contextmenu', saludar);
+
+function saludar() {
+    alert("Diste click derecho");
+}
 
 boton.addEventListener('dblclick', saludar);
 
 function saludar() {
-    alert('diste doble clic');
+    alert("Diste doble click");
+}*/
+
+boton.addEventListener('dblclick', saludar);
+
+function saludar() {
+    console.log("Diste doble click");
 }
